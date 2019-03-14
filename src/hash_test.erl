fae87dfc1d29aa7c13086fa797666dcfd0a6c257
Robyroc <33685936+Robyroc@users.noreply.github.com>
2019-03-14 15:43:46 +0100

    Added useful files

--- src/hash_test.erl
+++ src/hash_test.erl
@@ -14,7 +14,7 @@
 
 launch() ->
   X = [lists:flatten([integer_to_list(A), ".", integer_to_list(B), ".", integer_to_list(C), ".", integer_to_list(D),
-    ":", integer_to_list(6543)]) || A <- [192], B <- lists:seq(0, 255), C <- lists:seq(0, 255),
+    ":", integer_to_list(6543)]) || A <- [192, 5], B <- lists:seq(0, 255), C <- lists:seq(0, 255),
     D <- lists:seq(0,255)],
   Z = lists:map(fun(Y) -> crypto:hash(sha, Y) end, X),
   I = lists:map(fun(K) -> crypto:bytes_to_integer(K) end, Z),
