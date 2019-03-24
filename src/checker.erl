25dd8d2349fa64d5ef8ecf883ab6714b59530384
GiacomoZiffer <10532347@polimi.it>
2019-03-24 16:01:22 +0100

    Hash function and checker

---
+++ src/checker.erl
@@ -0,0 +1,157 @@
+-module(checker).
+-author("Giacomo").
+
+-behaviour(gen_server).
+
+%% API
+-export([start_link/4, pred_find/2]).
+
+%% gen_server callbacks
+-export([init/1,
+  handle_call/3,
+  handle_cast/2,
+  handle_info/2,
+  terminate/2,
+  code_change/3]).
+
+-define(SERVER, ?MODULE).
+-define(INTERVAL, 30000).
+
+-record(state, {pred, pred_id, own_id, n_bits}).
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Starts the server
+%%
+%% @end
+%%--------------------------------------------------------------------
+start_link(Predecessor, PredID, OwnID, NBits) ->            %%TODO decide how to pass these parameters
+  gen_server:start_link({local, ?SERVER}, ?MODULE, [Predecessor, PredID, OwnID, NBits], []).
+
+pred_find(PID, Address) ->
+  gen_server:call(PID, {pred_find, Address}).
+
+%%%===================================================================
+%%% gen_server callbacks
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Initializes the server
+%%
+%% @spec init(Args) -> {ok, State} |
+%%                     {ok, State, Timeout} |
+%%                     ignore |
+%%                     {stop, Reason}
+%% @end
+%%--------------------------------------------------------------------
+init([Predecessor, PredID, OwnID, NBits]) ->
+  naming_service:notify_identity(self(), checker),
+  {ok, #state{pred = Predecessor, pred_id = PredID, own_id = OwnID, n_bits = NBits}};
+
+init(_) ->
+  {stop, incorrect_params}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling call messages
+%%
+%% @end
+%%--------------------------------------------------------------------
+handle_call({pred_find, local_address}, _From, State) ->
+  {reply, State#state.pred , State, ?INTERVAL};
+
+handle_call({pred_find, Address}, _From, State) ->
+  case State#state.pred of
+    nil ->
+      PID = naming_service:get_identity(hash_f),
+      Index = hash_f:get_hashed_addr(PID, Address),
+      case Index of
+        _ when Index =< State#state.own_id ->
+          {reply, Address, #state{pred = Address, pred_id = Index}, ?INTERVAL};
+        _ when Index > State#state.own_id ->
+          CorrectIndex = Index - round(math:pow(2, State#state.n_bits)),
+          {reply, Address, #state{pred = Address, pred_id = CorrectIndex}, ?INTERVAL}
+      end,
+      {reply, Address, #state{pred = Address, pred_id = no_ID}, ?INTERVAL};
+    Predecessor ->
+      PID = naming_service:get_identity(hash_f),
+      Index = hash_f:get_hashed_addr(PID, Address),
+      #state{pred = Predecessor, pred_id = PredID, own_id = OwnID, n_bits = NBits} = State,
+      case Index of
+        _ when Index =< OwnID ->
+          {Addr, PredecessorID} = predecessor_chooser(Address, Index, Predecessor, PredID),
+          {reply, Addr, #state{pred = Addr, pred_id = PredecessorID}, ?INTERVAL};
+        _ when Index > OwnID ->
+          CorrectIndex = Index - round(math:pow(2, NBits)),
+          {Addr, PredecessorID} = predecessor_chooser(Address, CorrectIndex, Predecessor, PredID),
+          {reply, Addr, #state{pred = Addr, pred_id = PredecessorID}, ?INTERVAL}
+      end
+  end.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling cast messages
+%%
+%% @end
+%%--------------------------------------------------------------------
+handle_cast(Request, State) ->
+  io:format("FIX: Unexpected cast message: ~p~n", [Request]),
+  {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling all non call/cast messages
+%%
+%% @spec handle_info(Info, State) -> {noreply, State} |
+%%                                   {noreply, State, Timeout} |
+%%                                   {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_info(timeout, _State) ->
+  {noreply, #state{pred = nil, pred_id = nil}}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% This function is called by a gen_server when it is about to
+%% terminate. It should be the opposite of Module:init/1 and do any
+%% necessary cleaning up. When it returns, the gen_server terminates
+%% with Reason. The return value is ignored.
+%%
+%% @spec terminate(Reason, State) -> void()
+%% @end
+%%--------------------------------------------------------------------
+terminate(_Reason, _State) ->
+  ok.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Convert process state when code is changed
+%%
+%% @spec code_change(OldVsn, State, Extra) -> {ok, NewState}
+%% @end
+%%--------------------------------------------------------------------
+code_change(_OldVsn, State, _Extra) ->
+  {ok, State}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
+
+predecessor_chooser(Address, AddressID, Predecessor, PredecessorID) ->
+  case AddressID of
+    _ when AddressID > PredecessorID ->
+      {Address, AddressID};
+    _ when AddressID =< PredecessorID ->
+      {Predecessor, PredecessorID}
+  end.
---
+++ src/hash_f.erl
@@ -0,0 +1,127 @@
+-module(hash_f).
+-author("Giacomo").
+
+-behaviour(gen_server).
+
+%% API
+-export([start_link/1, get_hashed_addr/2, get_hashed_res/2]).
+
+%% gen_server callbacks
+-export([init/1,
+  handle_call/3,
+  handle_cast/2,
+  handle_info/2,
+  terminate/2,
+  code_change/3]).
+
+-define(SERVER, ?MODULE).
+
+-record(state, {nbits}).
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Starts the server
+%%
+%% @end
+%%--------------------------------------------------------------------
+start_link(NBits) ->
+  gen_server:start_link({local, ?SERVER}, ?MODULE, [NBits], []).
+
+get_hashed_addr(PID, Address) ->
+  gen_server:call(PID, {a_code, Address}).
+
+get_hashed_res(PID, Resource) ->
+  gen_server:call(PID, {r_code, Resource}).
+
+%%%===================================================================
+%%% gen_server callbacks
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Initializes the server
+%%
+%% @spec init(Args) -> {ok, State} |
+%%                     {ok, State, Timeout} |
+%%                     ignore |
+%%                     {stop, Reason}
+%% @end
+%%--------------------------------------------------------------------
+init([NBits]) ->
+  naming_service:notify_identity(self(), hash_f),
+  {ok, #state{nbits = NBits}}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling call messages
+%%
+%% @end
+%%--------------------------------------------------------------------
+handle_call({a_code, Address}, _From, State) ->
+  Compacted = link_manager:compact_address(Address),
+  Digest = crypto:bytes_to_integer(crypto:hash(sha, Compacted)),
+  Index = Digest rem round(math:pow(2, State#state.nbits)),
+  {reply, Index, State};
+
+handle_call({r_code, Resource}, _From, State) ->
+  Digest = crypto:bytes_to_integer(crypto:hash(sha, Resource)),
+  Index = Digest rem round(math:pow(2, State#state.nbits)),
+  {reply, Index, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling cast messages
+%%
+%% @end
+%%--------------------------------------------------------------------
+handle_cast(_Request, State) ->
+  {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling all non call/cast messages
+%%
+%% @spec handle_info(Info, State) -> {noreply, State} |
+%%                                   {noreply, State, Timeout} |
+%%                                   {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_info(_Info, State) ->
+  {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% This function is called by a gen_server when it is about to
+%% terminate. It should be the opposite of Module:init/1 and do any
+%% necessary cleaning up. When it returns, the gen_server terminates
+%% with Reason. The return value is ignored.
+%%
+%% @spec terminate(Reason, State) -> void()
+%% @end
+%%--------------------------------------------------------------------
+terminate(_Reason, _State) ->
+  ok.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Convert process state when code is changed
+%%
+%% @spec code_change(OldVsn, State, Extra) -> {ok, NewState}
+%% @end
+%%--------------------------------------------------------------------
+code_change(_OldVsn, State, _Extra) ->
+  {ok, State}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
