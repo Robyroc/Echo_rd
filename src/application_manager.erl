17c415d10e915b3c829e0959b73fa04ab640a408
GiacomoZiffer <10532347@polimi.it>
2019-04-22 11:32:55 +0200

    correction

--- src/application_manager.erl
+++ src/application_manager.erl
@@ -17,7 +17,6 @@
   create_p/2,
   connect/1,
   hash_name/1,
-  get_own_address/0,
   send_response/2]).
 
 %% gen_server callbacks
@@ -101,9 +100,6 @@ hash_name(Name) ->
     _ ->
       hash_f:get_hashed_name(Name)
   end.
-
-get_own_address() ->
-  link_manager:get_own_address().
 
 send_response(Message, Address) ->
   communication_manager:send_message_async(command, [Address, Message], Address, no_alias).
