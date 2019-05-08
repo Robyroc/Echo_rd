45f3f2024474e2a84cbfaf3d894f260746ed0d19
Antonio Urbano <antonio.urbano@mail.polimi.it>
2019-05-08 19:02:27 +0200

    Update for policies. It should be the last one for testing lager with policies

--- src/communication_manager.erl
+++ src/communication_manager.erl
@@ -90,7 +90,7 @@ handle_call({send_msg, Method, Params, Address, Alias}, _From, State) ->
     able_lager -> inout:info("### OUT ###: Method:~p | Params:~p | Address:~p \n", [Method, Params, Address]);
     able ->
       inout:info("### OUT ###: Method:~p | Params:~p | Address:~p \n", [Method, Params, Address]),
-      allPolicies:info("### OUT ###: Method:~p | Params:~p | Address:~p \n", [Method, Params, Address]);
+      lagerConsole:info("### OUT ###: Method:~p | Params:~p | Address:~p \n", [Method, Params, Address]);
     unable -> ok
   end,
   Translated = translate(Method),
@@ -120,7 +120,7 @@ handle_cast({send_msg, Method, Params, Address, Alias}, State) ->
   case logging_policies:check_policy(?MODULE) of
     able ->
       inout:info("### OUT ###: Method:~p | Params:~p | Address:~p~n", [Method, Params, Address]),
-      allPolicies:info("### OUT ###: Method:~p | Params:~p | Address:~p~n", [Method, Params, Address]);
+      lagerConsole:info("### OUT ###: Method:~p | Params:~p | Address:~p~n", [Method, Params, Address]);
     able_lager -> inout:info("### OUT ###: Method:~p | Params:~p | Address:~p~n", [Method, Params, Address]);
     unable -> ok
   end,
@@ -138,7 +138,7 @@ handle_cast({rcv_msg, Method, Address, Params}, State) ->
   DecodedParams = decode_params(back_translate(Method), Params, State#state.nbits),
   case logging_policies:check_policy(?MODULE) of
     able ->
-      allPolicies:info("### IN ###: Method:~p | Params:~p | Address:~p~n", [BackTranslated, DecodedParams, Address]),
+      lagerConsole:info("### IN ###: Method:~p | Params:~p | Address:~p~n", [BackTranslated, DecodedParams, Address]),
       inout:info("### IN ###: Method:~p | Params:~p | Address:~p~n", [BackTranslated, DecodedParams, Address]);
     able_lager -> inout:info("### IN ###: Method:~p | Params:~p | Address:~p~n", [BackTranslated, DecodedParams, Address]);
     unable -> ok
